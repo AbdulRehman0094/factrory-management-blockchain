f390d76288dfc6756c1ed26d1b17244610f29672
Noman Jafar <noman@thingtrax.com>
2024-04-07 01:22:13 +0500

    changes in smart contract

--- blockchain/interactions/ordersContract.js
+++ blockchain/interactions/ordersContract.js
@@ -13,7 +13,22 @@ async function placeOrder(productId, quantity, sellerAddress, buyerAddress, valu
     }
 }
 
+async function checkBalance(address) {
+    try {
+        const balance = await web3.eth.getBalance(address);
+        console.log('Contract balance:', web3.utils.fromWei(balance, 'ether'), 'ETH');
+    } catch (error) {
+        console.error('Error checking contract balance:', error);
+    }
+}
+
 // Call the placeOrder function
-const userAddress = '0xb992fc5c1bddd7314d214d8619c3e3cf8ef62165';
+const sellerAddress = '0xb992fc5c1bddd7314d214d8619c3e3cf8ef62165';
 const buyerAddress = '0xa6c9bbe3767e02100993ba13c509fb8cfbfcc3c6';
-placeOrder(1, 20, userAddress, buyerAddress, web3.utils.toWei('1', 'ether'));
+checkBalance(sellerAddress);
+checkBalance(buyerAddress);
+checkBalance('0x91AB7A3c4A1ca824eCa4a0B654DB408ca7e9787B');
+
+// placeOrder(1, 10, sellerAddress, buyerAddress, web3.utils.toWei('10', 'ether'));
+
+
